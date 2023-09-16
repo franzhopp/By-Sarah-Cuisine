12d6093d3314846ea5bc95610f97bfa32b1e5898
louohs <louisa.hadjili@epitech.eu>
2023-09-17 01:28:19 +0200

    [ADD] cookie

--- src/Components/Navigation/modal-cookie.jsx
+++ src/Components/Navigation/modal-cookie.jsx
@@ -8,7 +8,7 @@ const CookieModal = ({ isOpen, onRequestClose, onAccept }) => {
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       contentLabel="Cookie Modal"
-      className="fixed bottom-0 left-0 right-0 bg-white p-4"
+      className="fixed bottom-0 left-0 right-0 bg-white p-4 border-t border-gray-300 shadow-md"
       overlayClassName="fixed top-0 left-0 right-0 bottom-0 opacity-75"
     >
       <div className="text-left">
