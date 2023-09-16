fb1fb5eb001052cb287f89487637dc42a758e004
louohs <louisa.hadjili@epitech.eu>
2023-09-17 01:01:48 +0200

    [ADD] cookie

--- src/Components/Navigation/modal-cookie.jsx
+++ src/Components/Navigation/modal-cookie.jsx
@@ -8,17 +8,17 @@ const CookieModal = ({ isOpen, onRequestClose, onAccept }) => {
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       contentLabel="Cookie Modal"
-      className="fixed bottom-0 left-0 right-0 bg-white p-4 border-t border-gray-300 shadow-md"
+      className="fixed bottom-0 left-0 right-0 bg-white p-4 shadow-md"
       overlayClassName="fixed top-0 left-0 right-0 bottom-0 bg-gray-800 opacity-75"
     >
-      <div className="text-left">
+      <div className="text-left font-sans-serif">
         <h2 className="text-xl font-bold mb-2">Politique de cookies</h2>
         <p className="text-gray-600 mb-4">
           Ce site utilise des cookies pour améliorer votre expérience.
         </p>
         <button
           onClick={onAccept}
-          className=" bg-f3dbc3 hover:bg-gray text-white font-bold py-2 px-4 rounded-full"
+          className="bg-f3dbc3 hover:bg-gray text-white font-bold py-2 px-4 rounded-full"
         >
           Accepter
         </button>
