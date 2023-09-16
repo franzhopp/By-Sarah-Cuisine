34cae477ff0055a916e8e48790c24ec205293077
louohs <louisa.hadjili@epitech.eu>
2023-09-16 14:27:16 +0200

    [ADD] update cards client

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -4,9 +4,9 @@ import Image from "../../assets/svg-contact.png";
 const TitleForm = () => {
   return (
     <div>
-      <div className="image-contact-by-sarah-cuisine sm:py-20 sm:px-6 lg:px-8">
+      <div className="image-contact-by-sarah-cuisine sm:py-20 sm:px-24 lg:px-8">
         <span className="flex justify-center">
-          <img data-aos="fade-right" src={Image} alt="" className="mt-20 mb-20 sm:mb-10 sm:mt-4" />
+          <img data-aos="fade-right" src={Image} alt="" className="px-16 mt-24 mb-20 sm:mb-10 sm:mt-4" />
         </span>
       </div>
     </div>
--- src/Components/Partner/partner.jsx
+++ src/Components/Partner/partner.jsx
@@ -5,7 +5,7 @@ const Partner = () => {
     <div>
       <div className="image-client-by-sarah-cuisine mb-14 sm:py-20 sm:px-6 lg:px-8">
         <span className="flex justify-center">
-          <img data-aos="fade-right" src={Image} alt="" className="mt-20 mb-20 sm:mb-10 sm:mt-4" />
+          <img data-aos="fade-right" src={Image} alt="" className="px-16 mt-24 mb-20 sm:mb-10 sm:mt-4" />
         </span>
       </div>
     </div>
