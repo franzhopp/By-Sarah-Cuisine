d0ba47209efcc9a2426cf452f206eb8284012334
louohs <louisa.hadjili@epitech.eu>
2023-09-16 12:25:29 +0200

    [ADD] update

--- src/Components/Contact/form.jsx
+++ src/Components/Contact/form.jsx
@@ -1,3 +1,6 @@
+import { MdPhoneInTalk } from "react-icons/md";
+import { BsInstagram } from "react-icons/bs";
+
 const Form = () => {
   return (
     <section className="image-blur-by-sarah-cuisine">
@@ -10,30 +13,41 @@ const Form = () => {
           <div className="mb-10">
             <p className="font-sans-serif text-white">
               Remplissez le formulaire de contact ci-dessous afin qu’elle puisse
-              vous contacter pour toutes informations précises.
+              vous contacter pour toutes informations précises. Vous retrouvez tous ces réseaux afin d'avoir un contact direct en ligne mais pour plus de faciliter, veuillez remplir le formulaire.
             </p>
+            <div className="flex justify-center items-center space-x-2 mt-5">
+              <MdPhoneInTalk className="h-10 w-10 text-white" />
+              <p className="font-sans-serif text-white text-3xl font-extrabold">
+                07.81.86.39.81
+              </p>
+            </div>
+            <div className="flex justify-center items-center space-x-2 mt-5">
+              <BsInstagram className="h-10 w-10 text-white" />
+              <p className="font-sans-serif text-white text-3xl font-extrabold">
+                by_sarah_cuisine
+              </p>
+            </div>
           </div>
 
           <div className="bg-contact p-8 shadow-lg lg:col-span-3 lg:p-12 rounded-md mb-11">
             <form action="" className="space-y-4">
               <div action="" className="space-y-4">
-               
-                  <div>
-                    <input
-                      className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
-                      placeholder="Prénom"
-                      type="name"
-                    />
-                  </div>
+                <div>
+                  <input
+                    className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
+                    placeholder="Prénom"
+                    type="name"
+                  />
+                </div>
 
-                  <div>
-                    <input
-                      className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
-                      placeholder="Nom"
-                      type="name"
-                    />
-                  </div>
-                
+                <div>
+                  <input
+                    className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
+                    placeholder="Nom"
+                    type="name"
+                  />
+                </div>
+
                 <div>
                   <input
                     className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
