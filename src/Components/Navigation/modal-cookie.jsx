db5293c000eeb6fa3a212429419ba0db7ef9f20e
louohs <louisa.hadjili@epitech.eu>
2023-09-17 03:05:14 +0200

    [ADD] cookie

--- src/Components/Navigation/modal-cookie.jsx
+++ src/Components/Navigation/modal-cookie.jsx
@@ -1,6 +1,7 @@
-import React from "react";
 import Modal from "react-modal";
-Modal.setAppElement("#root");
+
+// Configuration du modal
+Modal.setAppElement("#root"); // Assurez-vous de définir l'élément racine de votre application
 
 const CookieModal = ({ isOpen, onRequestClose, onAccept }) => {
   return (
@@ -9,18 +10,6 @@ const CookieModal = ({ isOpen, onRequestClose, onAccept }) => {
       onRequestClose={onRequestClose}
       contentLabel="Cookie Modal"
     >
-      <div className="fixed bottom-0 left-0 right-0 bg-white h-64 w-64 p-4">
-        <h2 className="text-lg font-semibold">Politique de cookies</h2>
-        <p className="text-sm text-gray-600 mt-2">
-          Ce site utilise des cookies pour améliorer votre expérience.
-        </p>
-        <button
-          onClick={onAccept}
-          className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-full"
-        >
-          Accepter
-        </button>
-      </div>
     </Modal>
   );
 };
