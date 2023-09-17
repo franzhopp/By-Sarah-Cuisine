c5f7860a34f4f7fa7be57cb211e20ef650d03fae
louohs <louisa.hadjili@epitech.eu>
2023-09-17 14:18:37 +0200

    [ADD] cookie

--- src/Components/Navigation/cookie.jsx
+++ src/Components/Navigation/cookie.jsx
@@ -1,32 +1,32 @@
-import { useState, useEffect } from "react";
-import CookieModal from "./modal-cookie.jsx";
-import Cookies from "js-cookie";
+// import { useState, useEffect } from "react";
+// import CookieModal from "./modal-cookie.jsx";
+// import Cookies from "js-cookie";
 
-const Homepage = () => {
-  const [isCookieModalOpen, setCookieModalOpen] = useState(false);
+// const Homepage = () => {
+//   const [isCookieModalOpen, setCookieModalOpen] = useState(false);
 
-  useEffect(() => {
-    const hasAcceptedCookie = Cookies.get("accept_cookie");
-    if (!hasAcceptedCookie) {
-      setCookieModalOpen(true);
-    }
-  }, []);
+//   useEffect(() => {
+//     const hasAcceptedCookie = Cookies.get("accept_cookie");
+//     if (!hasAcceptedCookie) {
+//       setCookieModalOpen(true);
+//     }
+//   }, []);
 
-  const handleAcceptCookie = () => {
-    Cookies.set("accept_cookie", "true", { expires: 365 });
-    setCookieModalOpen(false);
-  };
+//   const handleAcceptCookie = () => {
+//     Cookies.set("accept_cookie", "true", { expires: 365 });
+//     setCookieModalOpen(false);
+//   };
 
-  return (
-    <div>
-      <CookieModal
-        isOpen={isCookieModalOpen}
-        onRequestClose={() => setCookieModalOpen(false)}
-        onAccept={handleAcceptCookie}
-      />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <CookieModal
+//         isOpen={isCookieModalOpen}
+//         onRequestClose={() => setCookieModalOpen(false)}
+//         onAccept={handleAcceptCookie}
+//       />
+//     </div>
+//   );
+// };
 
-export default Homepage;
+// export default Homepage;
 
