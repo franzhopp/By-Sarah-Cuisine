c651c89cf955cf735aa35bb779950b9bc8b2ecd2
louohs <louisa.hadjili@epitech.eu>
2023-09-18 13:09:59 +0200

    [ADD] svg home

--- src/Components/Section/section.jsx
+++ src/Components/Section/section.jsx
@@ -1,11 +1,19 @@
 const Section = () => {
   return (
-    <section
-      data-aos="fade-up"
-      data-aos-anchor-placement="top-center"
-      className="mb-12"
-    >
-      <div className="flex justify-center mt-16">
+    <section className="mb-12">
+      <div className="flex justify-center">
+        <svg
+          className="relative bottom-7"
+          width="748"
+          height="46"
+          viewBox="0 0 748 46"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect width="748" height="46" rx="20" fill="white" />
+        </svg>
+      </div>
+      <div className="flex justify-center ">
         <h2 className="font-extrabold text-2xl sm:text-4xl">
           <span className="text-pink font-sans-recursive">{`Traiteur de tous types d'évènements`}</span>
         </h2>
@@ -18,14 +26,22 @@ const Section = () => {
 
       <div className="flex justify-center">
         <div className="flex justify-center flex-col sm:flex-row mt-5">
-          <p className="text-black font-sans-serif w-80 m-4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
+          <p
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-center"
+            className="text-black font-sans-serif w-80 m-4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+          >
             {`Originaire de Meaux, Sarah est une excellente cusinière. Elle partage sa cuisine au quotidien,
             avec beaucoup de passion et de rigueur. L'excellence, l'hygiène, l'assiduité font partie de ses valeurs qu'elle prône.
             Elle crée des expériences uniques qui reflètent l'identité de ses services et ses 
             objectifs. Choisissez les formules qui conviennent à vos besoins.`}
           </p>
 
-          <p className="text-black font-sans-serif w-80 m-4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
+          <p
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-center"
+            className="text-black font-sans-serif w-80 m-4 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
+          >
             {`Originaire de Meaux, Sarah est une excellente cusinière. Elle partage sa cuisine au quotidien,
             avec beaucoup de passion et de rigueur. L'excellence, l'hygiène, l'assiduité font partie de ses valeurs qu'elle prône.
             Elle crée des expériences uniques qui reflètent l'identité de ses services et ses 
--- src/Pages/Devis/devis.jsx
+++ src/Pages/Devis/devis.jsx
@@ -2,151 +2,153 @@ import NavbarDevis from "../../Components/Navigation/navbar-devis";
 
 const Devis = () => {
   return (
-    <div className="bg-devis-color">
-      <section className="bg-devis">
+    <div>
+      <div className="bg-devis-color">
         <NavbarDevis />
-        <div className="mx-auto max-w-4xl px-4 py-28 sm:px-6 lg:px-8">
-          <h2 className="font-extrabold text-3xl text-center sm:text-5xl">
-            <span className="text-pink font-sans-recursive">{`Demander un devis sur-mesure !`}</span>
-          </h2>
+        <section className="bg-devis">
+          <div className="mx-auto max-w-4xl px-4 py-28 sm:px-6 lg:px-8">
+            <h2 className="font-extrabold text-3xl text-center sm:text-5xl">
+              <span className="text-pink font-sans-recursive">{`Demander un devis sur-mesure !`}</span>
+            </h2>
 
-          <div className="flex justify-center">
-            <div className="w-full sm:w-2/4">
-              <div className="text-black mt-12 mb-16 font-sans-serif text-base text-center">
-                Vous souhaitez préparer un évènement très important comme{" "}
-                <span className="font-extrabold">un mariage</span>,{" "}
-                <span className="font-extrabold">un anniversaire</span> ? Merci
-                de le préciser afin que le chef traite votre demande.
+            <div className="flex justify-center">
+              <div className="w-full sm:w-2/4">
+                <div className="text-black mt-12 mb-16 font-sans-serif text-base text-center">
+                  Vous souhaitez préparer un évènement très important comme{" "}
+                  <span className="font-extrabold">un mariage</span>,{" "}
+                  <span className="font-extrabold">un anniversaire</span> ?
+                  Merci de le préciser afin que le chef traite votre demande.
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="bg-devis rounded-lg">
-            <div className="bg-gray-100 p-8 shadow-lg lg:col-span-3 lg:p-12">
-              <form action="" className="space-y-4">
-                <div>
-                  <label className="sr-only" htmlFor="name">
-                    Prénom
-                  </label>
-                  <input
-                    className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
-                    placeholder="Prénom"
-                    type="text"
-                    id="name"
-                  />
-                </div>
-                <div>
-                  <label className="sr-only" htmlFor="name">
-                    Nom
-                  </label>
-                  <input
-                    className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
-                    placeholder="Nom"
-                    type="text"
-                    id="name"
-                  />
-                </div>
-
-                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+            <div className="bg-devis rounded-lg">
+              <div className="bg-gray-100 p-8 shadow-lg lg:col-span-3 lg:p-12">
+                <form action="" className="space-y-4">
                   <div>
-                    <label className="sr-only" htmlFor="email">
-                      Email
+                    <label className="sr-only" htmlFor="name">
+                      Prénom
                     </label>
                     <input
                       className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
-                      placeholder="Adresse mail"
-                      type="email"
-                      id="email"
+                      placeholder="Prénom"
+                      type="text"
+                      id="name"
                     />
                   </div>
-
                   <div>
-                    <label className="sr-only" htmlFor="phone">
-                      Téléphone
+                    <label className="sr-only" htmlFor="name">
+                      Nom
                     </label>
                     <input
                       className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
-                      placeholder="Téléphone"
-                      type="tel"
-                      id="phone"
+                      placeholder="Nom"
+                      type="text"
+                      id="name"
                     />
                   </div>
-                </div>
 
-                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                    <div>
+                      <label className="sr-only" htmlFor="email">
+                        Email
+                      </label>
+                      <input
+                        className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
+                        placeholder="Adresse mail"
+                        type="email"
+                        id="email"
+                      />
+                    </div>
+
+                    <div>
+                      <label className="sr-only" htmlFor="phone">
+                        Téléphone
+                      </label>
+                      <input
+                        className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2"
+                        placeholder="Téléphone"
+                        type="tel"
+                        id="phone"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                    <div>
+                      <label className="sr-only" htmlFor="email">
+                        Service choisi
+                      </label>
+                      <select
+                        className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2 bg-white text-zinc-400"
+                        placeholder="Service choisi"
+                        type="email"
+                        id="email"
+                      >
+                        <option selected>Service choisi</option>
+                        <option>Plats</option>
+                        <option>Desserts</option>
+                        <option>Évènements</option>
+                        <option>Mariage</option>
+                      </select>
+                    </div>
+
+                    <div>
+                      <label className="sr-only" htmlFor="phone">
+                        Date de la commande
+                      </label>
+                      <input
+                        className="rounded-xl w-full p-3 outline-none input-secondary border-gray-200 text-md mb-2 bg-white text-zinc-400"
+                        placeholder="Date de la commande"
+                        type="date"
+                        id="phone"
+                      />
+                    </div>
+                  </div>
+
                   <div>
-                    <label className="sr-only" htmlFor="email">
-                      Service choisi
-                    </label>
                     <select
                       className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2 bg-white text-zinc-400"
                       placeholder="Service choisi"
                       type="email"
                       id="email"
                     >
-                      <option selected>Service choisi</option>
-                      <option>Plats</option>
-                      <option>Desserts</option>
-                      <option>Évènements</option>
-                      <option>Mariage</option>
+                      <option selected>{`Comment m'avez-vous connu ?`}</option>
+                      <option>Par un proche</option>
+                      <option>Sur internet</option>
+                      <option>Les réseaux sociaux</option>
+                      <option>Client régulier</option>
                     </select>
                   </div>
 
+                  {/* Message */}
                   <div>
-                    <label className="sr-only" htmlFor="phone">
-                      Date de la commande
+                    <label className="sr-only" htmlFor="message">
+                      Message
                     </label>
-                    <input
-                      className="rounded-xl w-full p-3 outline-none input-secondary border-gray-200 text-md mb-2 bg-white text-zinc-400"
-                      placeholder="Date de la commande"
-                      type="date"
-                      id="phone"
-                    />
+
+                    <textarea
+                      className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2 bg-white"
+                      placeholder="Message"
+                      rows="8"
+                      id="message"
+                    ></textarea>
                   </div>
-                </div>
 
-                <div>
-                  <select
-                    className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2 bg-white text-zinc-400"
-                    placeholder="Service choisi"
-                    type="email"
-                    id="email"
-                  >
-                    <option selected>{`Comment m'avez-vous connu ?`}</option>
-                    <option>Par un proche</option>
-                    <option>Sur internet</option>
-                    <option>Les réseaux sociaux</option>
-                    <option>Client régulier</option>
-                  </select>
-                </div>
-
-                {/* Message */}
-                <div>
-                  <label className="sr-only" htmlFor="message">
-                    Message
-                  </label>
-
-                  <textarea
-                    className="rounded-xl w-full p-4 outline-none input-secondary border-gray-200 text-md mb-2 bg-white"
-                    placeholder="Message"
-                    rows="8"
-                    id="message"
-                  ></textarea>
-                </div>
-
-                <div className="flex justify-center items-center mt-4">
-                  <button
-                    type="submit"
-                    className="bg-f3dbc3 shadow-md px-6 py-2 border rounded-3xl text-white"
-                  >
-                    Envoyer
-                  </button>
-                </div>
-              </form>
+                  <div className="flex justify-center items-center mt-4">
+                    <button
+                      type="submit"
+                      className="bg-f3dbc3 shadow-md px-6 py-2 border rounded-3xl text-white"
+                    >
+                      Envoyer
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
