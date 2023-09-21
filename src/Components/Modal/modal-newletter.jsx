7cc5ad332004c0844f1edeb49b2ee3aadcb7e959
louohs <louisa.hadjili@epitech.eu>
2023-09-21 12:29:16 +0200

    [ADD] modal newletter

--- src/Components/Modal/modal-newletter.jsx
+++ src/Components/Modal/modal-newletter.jsx
@@ -15,7 +15,7 @@ const ModalNewletter = ({ closeModal, searchQuery }) => {
             <ImCross className="text-C22E2E mb-4 sm:mb-0" />
           </button>
         </div>
-        <div className="mt-5">
+        <div className="flex justify-center mt-2">
           <button
             id="navlinks"
             className="bg-f3dbc3 shadow-md px-6 py-2 border rounded-3xl text-white hover:bg-e9c2c2"
