f2caf5c6ac07b1e6dd07e3c12bf52e4b75c0b69e
louohs <louisa.hadjili@epitech.eu>
2023-09-26 15:09:12 +0200

    [ADD] change img and title from services

--- src/Pages/Services/services.jsx
+++ src/Pages/Services/services.jsx
@@ -1,7 +1,19 @@
 import { NavLink } from "react-router-dom";
 import NavbarServices from "../../Components/Navigation/navbar-services";
 import Image from "../../assets/template.png";
-import { MdOutlineEditNote } from "react-icons/md";
+import { MdOutlineEditNote, MdOutlineFileDownload } from "react-icons/md";
+import ImgPiecesDeTousGenres from "../../assets/piecesdetousgenres.png";
+import ImgVerrines from "../../assets/verrines.png";
+import ImgToast from "../../assets/toast.png";
+import ImgCouscous from "../../assets/couscous.png";
+import ImgPaella from "../../assets/paella.png";
+import ImgTajine from "../../assets/tajine.png";
+import ImgCrepes from "../../assets/crepes.png";
+import ImgDonuts from "../../assets/donuts.png";
+import ImgViennoiseries from "../../assets/viennoiseries.png";
+import ImgMariage from "../../assets/mariage.png";
+import ImgAnniversaire from "../../assets/anniversaire.png";
+import ImgSoiree from "../../assets/soiree.png";
 
 const Services = () => {
   return (
@@ -22,11 +34,18 @@ const Services = () => {
         className="flex flex-wrap justify-center items-center space-x-0 sm:space-x-6"
       >
         {/* Step 1 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Pièces
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgPiecesDeTousGenres}
+              alt="Image apéritifs"
+            />
+          </div>
+
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Pièces de différentes formules
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -37,11 +56,17 @@ const Services = () => {
         </div>
 
         {/* Step 2 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Verrine
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgToast}
+              alt="Image apéritifs"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Toast
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -52,11 +77,17 @@ const Services = () => {
         </div>
 
         {/* Step 3 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Pièces
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgVerrines}
+              alt="Image apéritifs"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Verrines
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -66,6 +97,7 @@ const Services = () => {
           </div>
         </div>
       </div>
+
       <div className="max-w-7xl mt-10 mx-auto py-15 px-4 pb-10 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-extrabold text-right tracking-tighter">
           <p className="text-pink font-sans-recursive text-4xl sm:text-5xl ">
@@ -81,11 +113,17 @@ const Services = () => {
         className="flex flex-wrap justify-center items-center space-x-0 sm:space-x-6"
       >
         {/* Step 1 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Verrine
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgCouscous}
+              alt="Image plats"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Couscous
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -96,11 +134,17 @@ const Services = () => {
         </div>
 
         {/* Step 2 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Verrine
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgPaella}
+              alt="Image Plats"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Paëlla
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -111,11 +155,17 @@ const Services = () => {
         </div>
 
         {/* Step 3 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Verrine
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgTajine}
+              alt="Image plats"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Tajine
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -125,27 +175,34 @@ const Services = () => {
           </div>
         </div>
       </div>
+
       <div className="max-w-7xl mt-10 mx-auto py-15 px-4 pb-10 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-extrabold text-color-gray-pastel text-left tracking-tighter">
           <p className="text-pink font-sans-recursive text-4xl sm:text-5xl ">
-            Desserts
+            Sucrées
           </p>
           <p className="text-black font-sans-serif text-base sm:text-2xl">
             Préparation d’apéritifs pour vos entrées
           </p>
         </h1>
       </div>
-      {/* Step 1 */}
+
       <div
         data-aos="fade-up"
         className="mb-20 flex flex-wrap justify-center items-center space-x-0 sm:space-x-6"
       >
         {/* Step 1 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Verrine
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgCrepes}
+              alt="Image sucrées"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Crêpes
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -156,11 +213,17 @@ const Services = () => {
         </div>
 
         {/* Step 2 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Verrine
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgDonuts}
+              alt="Image sucrées"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Donuts
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -171,11 +234,17 @@ const Services = () => {
         </div>
 
         {/* Step 3 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Verrine
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgViennoiseries}
+              alt="Image sucrées"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Viennoiseries
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -185,6 +254,7 @@ const Services = () => {
           </div>
         </div>
       </div>
+
       <div className="max-w-7xl mt-10 mx-auto py-15 px-4 pb-10 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-extrabold text-right tracking-tighter">
           <p className="text-pink font-sans-recursive text-4xl sm:text-5xl ">
@@ -195,16 +265,23 @@ const Services = () => {
           </p>
         </h1>
       </div>
-      {/* Step 1 */}
+
       <div
-        data-aos="fade-up" className="mb-20 flex flex-wrap justify-center items-center space-x-0 sm:space-x-6"
+        data-aos="fade-up"
+        className="mb-20 flex flex-wrap justify-center items-center space-x-0 sm:space-x-6"
       >
         {/* Step 1 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Verrine
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgMariage}
+              alt="Image évènements"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Mariage
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -215,11 +292,17 @@ const Services = () => {
         </div>
 
         {/* Step 2 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Verrine
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgAnniversaire}
+              alt="Image évènements"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Anniversaire
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -230,11 +313,17 @@ const Services = () => {
         </div>
 
         {/* Step 3 */}
-        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition ease duration-300 group hover:translate-y-5">
-          <img className="w-full p-10" src={Image} alt="Pictures apéritifs" />
-          <div className="px-6 py-4">
-            <div className="font-bold text-pink underline text-xl mb-2">
-              Verrine
+        <div className="max-w-sm rounded-3xl overflow-hidden shadow-lg pt-5 pb-5 transition-transform transform hover:scale-105">
+          <div className="p-10">
+            <img
+              className="w-full rounded-xl"
+              src={ImgSoiree}
+              alt="Image apéritifs"
+            />
+          </div>
+          <div className="px-6 py-3">
+            <div className="font-bold text-pink font-sans-recursive underline text-3xl mb-2">
+              Soirées entre vos familles, amis
             </div>
             <p className="text-black font-semibold text-base font-sans-serif">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -244,15 +333,16 @@ const Services = () => {
           </div>
         </div>
       </div>
-        <div className="px-12 mt-12 mb-16 text-black text-base text-center font-sans-serif font-extrabold">
+
+      <div className="px-12 mt-12 mb-16 text-black text-base text-center font-sans-serif font-extrabold">
         Un devis sur-mesure ? Pour préparer un évènement, c'est par ici.
         <div className="mt-5 flex justify-center">
           <NavLink
-             to="/devisgratuit"
-             onClick={() => {
-               window.location.href = "/devisgratuit";
-               window.scrollTo(0, 0);
-             }}
+            to="/devisgratuit"
+            onClick={() => {
+              window.location.href = "/devisgratuit";
+              window.scrollTo(0, 0);
+            }}
             href="http://by-sarah-cuisine.netlify.app/devis-gratuit"
             className="bg-e9c2c2 shadow-md px-6 py-2 border rounded-3xl text-white hover:bg-f3dbc3"
           >
@@ -260,6 +350,20 @@ const Services = () => {
               Devis gratuit <MdOutlineEditNote className="ml-2 mt-1" />
             </p>
           </NavLink>
+        </div>
+      </div>
+      <div className="image-bg py-5 px-6">
+        <div className="px-12 mt-12 mb-16 text-pink font-sans-recursive text-3xl text-center font-extrabold">
+          Télécharger le flyer de Sarah ainsi que sa carte de visite en un clic
+          !
+          <div className="flex justify-center">
+            <a href="/resume/cdvfyr.png" download>
+              <span className="sr-only">Download flyer</span>
+              <button className="bg-e9c2c2 mt-5 shadow-md border text-white px-6 py-2 rounded-3xl hover:bg-f3dbc3">
+                <MdOutlineFileDownload className="text-white text-3xl" />
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </>
