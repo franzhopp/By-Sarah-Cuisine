a65ac999ca4aaedf5fda11023c369fcfc70f4dde
louohs <louisa.hadjili@epitech.eu>
2023-10-21 02:58:40 +0200

    [UPDATE] code navbar homepage

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -3,19 +3,19 @@ import Image from "../../assets/title-home.png";
 const Homepage = () => {
   return (
     <section id="home">
-      <div className="flex flex-col justify-center h-my-screen pt-52 sm:pt-32">
+      <div className="flex flex-col justify-center h-my-screen pt-40 sm:pt-32">
         <p
           data-aos="fade-in"
           className="text-center text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-beige font-sans-recursive stroke"
         >
           By Sarah Cuisine
         </p>
-        <div className="flex justify-center">
+        <div className="flex justify-center mt-2">
           <img
             data-aos="fade-down-right"
             src={Image}
             alt="Icône bysarahcuisine"
-            className="px-3"
+            className="px-6"
           />
         </div>
       </div>
