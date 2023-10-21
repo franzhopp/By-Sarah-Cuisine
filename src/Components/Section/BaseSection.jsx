62b6e8cbd4c5bc778a87e52e0b2958d891237103
louohs <louisa.hadjili@epitech.eu>
2023-10-21 13:21:43 +0200

    [UPDATE] svg nav icons

--- src/Components/Section/BaseSection.jsx
+++ src/Components/Section/BaseSection.jsx
@@ -76,7 +76,7 @@ const Section = () => {
             pour devenir une véritable experte en cuisine. »
             <div className="mt-5 mb-5">
               <p className="uppercase font-extrabold text-gray text-sm tracking-widest">
-                ― Par Sarah, le 01/10/23.
+              👩‍🍳 Par Sarah, le 01/10/23.
               </p>
             </div>
           </p>
